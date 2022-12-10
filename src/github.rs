a67aa3d07673b95d9f4effb7efd419cc0eabaff7
Daniel Bright <brahyt@gmail.com>
2022-12-10 10:55:29 -0800

    added github mod

---
+++ src/github.rs
@@ -0,0 +1,5 @@
+pub fn create_branch(name: &str) {
+    if name.is_empty() { println!("Name cannot be empty"); return }
+
+    println!("Creating Branch: {}", name);
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
-fn main() {
-    println!("Hello, world!");
+use serde_json::{Value};
+mod github;
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let resp = reqwest::get("https://swapi.dev/api/people/1")
+        .await?
+        .text()
+        .await?;
+
+    let deserialized: Value = serde_json::from_str(&resp)?;
+
+    github::create_branch("");
+
+    println!("{:#?}", deserialized);
+    Ok(())
 }
